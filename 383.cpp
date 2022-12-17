34a5166c475941b28cc51af61bb94c88fa66a85f
Igwe Dev <ofolympus@OS>
2022-12-17 11:34:48 -0500

    383.cpp

---
+++ 383.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+       unordered_map<char, int> m; 
+       
+       for(int i = 0; i < ransomNote.size(); i ++) m[ransomNote[i]]++;
+       for(int i = 0; i < magazine.size(); i ++) m[magazine[i]]--;
+       for(int i = 0; i < ransomNote.size(); i ++) {
+           if(m[ransomNote[i]] > 0) return 0;
+       }
+       return 1;
+    }
+};
